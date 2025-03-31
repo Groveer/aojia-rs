7249d9679c510a687bd0c51facab72088a739a1d
groveer <groveer@outlook.com>
2025-03-31 21:40:55 +0800

    add some func

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ fn main() {
     let aojia = AoJia::new_with_path(String::from("ARegJ64.dll"), String::from("AoJia64.dll")).unwrap();
     println!("插件版本：{}", aojia.VerS().unwrap());
 
-    let ret = aojia.SetPath("\\").unwrap();
-    println!("SetPath ret: {}", ret);
+    let ret = aojia.GetMachineCode().unwrap();
+    println!("GetMachineCode ret: {}", ret);
+
+    let mut v = String::new();
+    let mut vn = String::new();
+    let mut vbn = -1;
+    let mut dir = String::new();
+
+    let ret = aojia.GetOs(&mut v, &mut vn, &mut vbn, &mut dir, 0).unwrap();
+    println!("GetOs ret: {}, v: {}, vn: {}, vbn: {}, dir: {}", ret, v, vn, vbn, dir);
 }
